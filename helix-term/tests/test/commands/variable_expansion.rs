c2172847b2a55d0204e541ab5521bc15aa7e971b
Théo Daron <theo@daron.be>
2024-07-30 23:03:22 +0200

    adding tests

--- helix-term/tests/test/commands/variable_expansion.rs
+++ helix-term/tests/test/commands/variable_expansion.rs
@@ -1,5 +1,5 @@
 use super::*;
-
+use std::borrow::Cow;
 #[tokio::test(flavor = "multi_thread")]
 async fn test_variable_expansion() -> anyhow::Result<()> {
     {
@@ -17,8 +17,6 @@ async fn test_variable_expansion() -> anyhow::Result<()> {
             false,
         )
         .await?;
-        let mut app = AppBuilder::new().build()?;
-
         let mut app = AppBuilder::new().build()?;
 
         test_key_sequence(
@@ -124,6 +122,57 @@ async fn test_variable_expansion() -> anyhow::Result<()> {
             Some("ihelix<ret>helix<ret>helix<ret><esc>:echo %{linenumber}<ret>"),
             Some(&|app| {
                 assert_eq!(app.editor.get_status().unwrap().0, "4");
+            }),
+            false,
+        )
+        .await?;
+
+        let mut app = AppBuilder::new().build()?;
+
+        test_key_sequence(
+            &mut app,
+            Some("<esc>:echo %sh{echo %{filename}}<ret>"),
+            Some(&|app| {
+                assert_eq!(
+                    app.editor.get_status().unwrap().0,
+                    helix_view::document::SCRATCH_BUFFER_NAME
+                );
+            }),
+            false,
+        )
+        .await?;
+        let mut app = AppBuilder::new().build()?;
+
+        test_key_sequence(
+            &mut app,
+            Some("<esc>:echo %sh{echo %{filename} %{linenumber}}<ret>"),
+            Some(&|app| {
+                assert_eq!(
+                    app.editor.get_status().unwrap().0,
+                    &Cow::from(format!(
+                        "{} {}",
+                        helix_view::document::SCRATCH_BUFFER_NAME,
+                        1
+                    ))
+                );
+            }),
+            false,
+        )
+        .await?;
+        let mut app = AppBuilder::new().build()?;
+
+        test_key_sequence(
+            &mut app,
+            Some("<esc>:echo %sh{echo %{filename} %sh{echo %{filename}}}<ret>"),
+            Some(&|app| {
+                assert_eq!(
+                    app.editor.get_status().unwrap().0,
+                    &Cow::from(format!(
+                        "{} {}",
+                        helix_view::document::SCRATCH_BUFFER_NAME,
+                        helix_view::document::SCRATCH_BUFFER_NAME
+                    ))
+                );
             }),
             false,
         )
